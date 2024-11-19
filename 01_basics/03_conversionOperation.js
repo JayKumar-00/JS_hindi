f709c9c4374940f27f6fc8bf7a0f38aa5cd14afe
JayKumar-00 <kumjay93@gmail.com>
2024-11-19 15:13:40 +0000

    conversion complete

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,50 @@
+// let score = true
+// console.log(typeof score); //OUTPUT - number
+// console.log(typeof (score)); //OUTPUT - 1
+
+
+// let score = "33"
+
+// console.log(typeof score); //OUTPUT - string
+// console.log(typeof (score)); //OUTPUT - string
+
+
+// let score = "User_Jay"
+
+// console.log(typeof score); //OUTPUT - string
+// console.log(typeof (score)); //OUTPUT - Nan
+
+
+// let valueInNumber = Number(score)
+// // let score = "33abc"
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // Not a Number (NaN)
+
+// let score = null
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let score = undefined
+// console.log(typeof score); //OUTPUT - number
+// console.log(typeof (score)); //OUTPUT - 0
+
+
+//"33" =>  33
+//"33abc" => NaN, But type is number
+// "true" => 1 ; false => 0
+
+// let isLoggedIn = 1 //OUTPUT - true
+// let isLoggedIn = "" //OUTPUT - false
+// let isLoggedIn = "hitesh" //OUTPUT - true
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//1 => true ; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+let strinNumber = String(someNumber)
+console.log(strinNumber); //OUTPUT - 33
+console.log(typeof strinNumber); //OUTPUT - string
