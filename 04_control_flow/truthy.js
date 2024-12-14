89ebb52a79bef9a25115f5e0caa027e381a9584c
JayKumar-00 <kumjay93@gmail.com>
2024-12-14 05:52:12 +0000

    Nullish coalescing  & Ternary Operator....

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -25,3 +25,21 @@ if(Object.keys(emptyObj).length === 0){ //because it will give value as array
         console.log("Object is Empty ");
 
 }
+
+//Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 15 ?? 20
+
+console.log(val1);
+
+//Ternary Operator
+
+// condition ? true : false
+
+const iceTea = 100
+iceTea <= 80 ? console.log("less than 80") : console.log("more than 80");
+
